88afb4cf3c7bbc5969138f91bb91192f714d4473
donfortune <donfortunet.df@gmail.com>
2025-02-17 09:10:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const env = require('dotenv');
+const mongoose = require('mongoose');
+
+env.config();
+const app = express();
+
+app.use(express.json()); // to parse the incoming request with JSON payloads
+
+const connectDb = () => {
+    try {
+        mongoose.connect(process.env.MONGO_URI, {
+            
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // useCreateIndex: true,
+            
+        })
+        console.log('Connected to database');
+    } catch (error) {
+        console.log('Error connecting to database', error);
+    }
+}
+
+connectDb();
+
+
+port = process.env.PORT
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
